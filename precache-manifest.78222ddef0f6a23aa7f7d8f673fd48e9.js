5e64058b51d3bba75a65f0feec1e82f8468f73df
SAMRATHDEEPSINGH <samrathdeepsingh19@gmail.com>
2023-07-27 01:51:11 +0530

    Updates

---
+++ precache-manifest.78222ddef0f6a23aa7f7d8f673fd48e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc1053ae515ced1adf85",
+    "url": "/TEXTUTILS/static/css/main.5cc2a1dd.chunk.css"
+  },
+  {
+    "revision": "bc1053ae515ced1adf85",
+    "url": "/TEXTUTILS/static/js/main.9fe3cb8c.chunk.js"
+  },
+  {
+    "revision": "86b948c5925f26843064",
+    "url": "/TEXTUTILS/static/js/runtime~main.b23a8e01.js"
+  },
+  {
+    "revision": "017cf2878163aa4650c1",
+    "url": "/TEXTUTILS/static/js/2.f4b306fe.chunk.js"
+  },
+  {
+    "revision": "f54d1ac797f637aac8ec",
+    "url": "/TEXTUTILS/static/js/3.b359b5d6.chunk.js"
+  },
+  {
+    "revision": "1892771d98c92b24dc55bb5acba2c9f2",
+    "url": "/TEXTUTILS/index.html"
+  }
+];
